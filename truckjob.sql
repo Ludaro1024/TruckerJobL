47a6d44d99155b95658cb4a86a39ad700f1c259a
ludaro <92580232+waschmaschvanlu@users.noreply.github.com>
2022-06-11 13:03:24 +0200

    Update truckjob.sql

--- truckjob.sql
+++ truckjob.sql
@@ -1,2 +1,2 @@
-ALTER TABLE `users` ADD COLUMN `truckerexp` int(11) NOT NULL DEFAULT 1,
+ALTER TABLE `users` ADD COLUMN `truckerexp` int(11) NOT NULL DEFAULT 1;
 ALTER TABLE `users` ADD COLUMN `truck` varchar(60)
